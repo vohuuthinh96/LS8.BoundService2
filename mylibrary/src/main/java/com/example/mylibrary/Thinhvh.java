3b99e8f23059428426d7cfece7d33e9d4b77186a
thinhvh <thinhvohuu@git.sohacorp.vn>
2022-03-17 15:46:21 +0700

    Added the submodule to the project.

---
+++ mylibrary/src/main/java/com/example/mylibrary/Thinhvh.java
@@ -0,0 +1,9 @@
+package com.example.mylibrary;
+/**
+* Created by : Thinhvh on 3/17/2022
+* Phone      : 0962890153 - 0398477967
+* Facebook   : https://www.facebook.com/thinh.super22
+* Skype      : https://join.skype.com/invite/fvfRTDLcGPJN
+* Mail       : thinhvhph04204@gmail.com
+*/public    class Thinhvh   {
+}
